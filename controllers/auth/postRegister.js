ee746d281f04fea75f9ca228d8fc5ff0922e3e9a
Saloniaga <sagarwal9475@gmail.com>
2022-08-17 20:36:14 +0530

    login register route

--- controllers/auth/postRegister.js
+++ controllers/auth/postRegister.js
@@ -1,4 +1,34 @@
+const User = require("../../models/user");
+const bcrypt = require("bcryptjs");
+
 const postRegister = async (req, res) => {
-  res.send("register route");
+  try {
+    const { username, mail, password } = req.body;
+
+    //checking if mail already exists
+    const userExists = await User.exists({ mail: mail.toLowerCase() });
+
+    if (userExists) {
+      return res.status(409).send("E-mail already in use.");
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      mail: mail.toLowerCase(),
+      password: encryptedPassword,
+    });
+    const token = "JWT_TOKEN";
+    res.status(201).json({
+      userDetails: {
+        mail: user.mail,
+        token: token,
+        username: user.username,
+      },
+    });
+  } catch (err) {
+    return res.send(500).send("ERROR OCCURED. TRY AGAIN.");
+  }
 };
 module.exports = postRegister;
