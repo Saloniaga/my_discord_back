0abbf29b11ad82055dc2bb8b952d96d4b7d297bf
Saloniaga <sagarwal9475@gmail.com>
2022-09-09 00:21:43 +0530

    leave room event handled

--- socketHandlers/disconnectHandler.js
+++ socketHandlers/disconnectHandler.js
@@ -1,6 +1,18 @@
 const serverStore = require("../serverStore");
+const roomLeaveHandler = require("./roomLeaveHandler");
 
 const disconnectHandler = (socket) => {
+  const activeRooms = serverStore.getActiveRooms();
+
+  activeRooms.forEach((activeRoom) => {
+    const userInRoom = activeRoom.participants.some(
+      (participant) => participant.socketId === socket.id
+    );
+
+    if (userInRoom) {
+      roomLeaveHandler(socket, { roomId: activeRoom.roomId });
+    }
+  });
   serverStore.removeConnectedUser(socket.id);
 };
 
